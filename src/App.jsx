6da40ede11e3550d7e0bfcab66c21592fb0cb6f7
Flory <flory.contato@gmail.com>
2024-10-10 20:29:37 -0300

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css'
+import { SearchUserInput } from "./components/search-user"
+
+function App() {
+
+  return (
+    <main className={styles.App}>
+      <SearchUserInput/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/search-user/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './styles.module.css'
+import { Search } from 'lucide-react'
+
+export function SearchUserInput() {
+  return (
+    <div className={styles.container}>
+      <Search className={styles.iconSearch} size={30}/>
+      <input/>
+      <button>Search</button>
+    </div>
+  )
+}
